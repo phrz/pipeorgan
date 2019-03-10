995ea96f1f48cd6ed0a39e94c25d94b24131e872
phrz <mail@paulherz.com>
2019-03-10 14:50:10 -0500

    Initial Commit

---
+++ Music/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Music
+//
+//  Created by Paul Herz on 2019-03-10.
+//  Copyright © 2019 Paul Herz. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	std::cout << "Hello, World!\n";
+	return 0;
+}
